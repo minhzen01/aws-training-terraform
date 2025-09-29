855b3119101f736e4a178cfd4bbfc7839774da75
minhzen01 <minh.zen1@gmail.com>
2025-09-29 17:08:21 +0700

    private route table

--- source/vpc/main.tf
+++ source/vpc/main.tf
@@ -86,8 +86,9 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private[*].id)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
+
 
 # Security Group
 resource "aws_security_group" "bastion_sg" {
