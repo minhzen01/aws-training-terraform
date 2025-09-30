f3d837e9f40acac05930859c9e76ed2690bcef33
minhzen01 <minh.zen1@gmail.com>
2025-09-30 14:35:20 +0700

    db

--- dev/variables.tf
+++ dev/variables.tf
@@ -11,9 +11,3 @@ variable "ami_id_db" {}
 
 variable "instance_type_bastion" {}
 variable "instance_type_db" {}
-
-variable "subnet_id_bastion" {}
-variable "subnet_id_db" {}
-
-variable "vpc_security_group_id_bastion" {}
-variable "vpc_security_group_id_db" {}
