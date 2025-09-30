82a9f5d4561211f1a6ad78d1918052e490c552ec
minhzen01 <minh.zen1@gmail.com>
2025-09-30 15:40:52 +0700

    egress web to db

--- source/vpc/main.tf
+++ source/vpc/main.tf
@@ -153,6 +153,15 @@ resource "aws_security_group_rule" "web_from_bastion" {
   security_group_id        = aws_security_group.web_sg.id
 }
 
+resource "aws_security_group_rule" "web_to_db" {
+  type                     = "egress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.db_sg.id
+  security_group_id        = aws_security_group.web_sg.id
+}
+
 resource "aws_security_group_rule" "db_from_bastion" {
   type                     = "ingress"
   from_port                = 8080
