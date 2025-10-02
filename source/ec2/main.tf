6a5a5aa544fdc882b73cea66170f4b256dc39e90
minhzen01 <minh.zen1@gmail.com>
2025-10-02 15:28:51 +0700

    healthcheck

--- source/ec2/main.tf
+++ source/ec2/main.tf
@@ -113,6 +113,16 @@ resource "aws_lb_target_group" "web_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200-399"
+    path                = "/index.php"
+  }
 }
 
 # ALB 
